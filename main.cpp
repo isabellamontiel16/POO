045fbf708ac87bf5aa053d7e6d8d343a9896c7c5
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:18:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 #include "ProductoLimpieza.h"
 
 int main() {
-    vector<Producto*> inventario;
+    std::vector<Producto*> inventario;
 
     inventario.push_back(new ProductoPerecedero(1, "Leche entera", 22.5, 10, "2025-07-01"));
     inventario.push_back(new ProductoNoPerecedero(2, "Arroz integral", 18.0, 5, "SuperGrano"));
     inventario.push_back(new ProductoBebida(3, "Vino tinto", 120.0, 2, true));
     inventario.push_back(new ProductoLimpieza(4, "Cloro multiusos", 25.0, 4, "Baños"));
 
-    cout << "Inventario del supermercado:\n\n";
+    std::cout << "Inventario del supermercado:\n\n";
     for (auto p : inventario) {
-        cout << p->to_string() << endl;
+        std::cout << *p << std::endl;
     }
 
     // Liberar memoria
