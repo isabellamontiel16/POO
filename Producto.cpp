0896ecd16fdbc4f87c2b9c9702190cd900f8e772
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:19:37 -0600

    Update Producto.cpp

--- Producto.cpp
+++ Producto.cpp
@@ -1,8 +1,12 @@
 #include "Producto.h"
-#include <sstream>
 
 Producto::Producto() : id(0), nombre(""), tipo("") {}
 
-Producto::Producto(int _id, const string& _nombre, const string& _tipo)
+Producto::Producto(int _id, const std::string& _nombre, const std::string& _tipo)
     : id(_id), nombre(_nombre), tipo(_tipo) {
+}
+
+std::ostream& operator<<(std::ostream& os, const Producto& p) {
+    os << p.to_string();
+    return os;
 }
