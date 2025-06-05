b0f721f26f434b83f122239093a43f7399726315
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:21:36 -0600

    Update ProductoLimpieza.cpp

--- ProductoLimpieza.cpp
+++ ProductoLimpieza.cpp
@@ -1,12 +1,12 @@
 #include "ProductoLimpieza.h"
 #include <sstream>
 
-ProductoLimpieza::ProductoLimpieza(int _id, const string& _nombre, double _precio, int _cantidad, const string& _superficie)
+ProductoLimpieza::ProductoLimpieza(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _superficie)
     : Producto(_id, _nombre, "Limpieza"), precio(_precio), cantidad(_cantidad), superficie(_superficie) {
 }
 
-string ProductoLimpieza::to_string() const {
-    stringstream ss;
+std::string ProductoLimpieza::to_string() const {
+    std::stringstream ss;
     ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
         << ", Precio: " << precio << ", Cantidad: " << cantidad
         << ", Superficie: " << superficie;
