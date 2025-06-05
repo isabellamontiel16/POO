a07f878d435c873d4a0ec041b4968082c91a54e5
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:22:05 -0600

    Update ProductoNoPerecedero.cpp

--- ProductoNoPerecedero.cpp
+++ ProductoNoPerecedero.cpp
@@ -1,12 +1,12 @@
 #include "ProductoNoPerecedero.h"
 #include <sstream>
 
-ProductoNoPerecedero::ProductoNoPerecedero(int _id, const string& _nombre, double _precio, int _cantidad, const string& _marca)
+ProductoNoPerecedero::ProductoNoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _marca)
     : Producto(_id, _nombre, "No Perecedero"), precio(_precio), cantidad(_cantidad), marca(_marca) {
 }
 
-string ProductoNoPerecedero::to_string() const {
-    stringstream ss;
+std::string ProductoNoPerecedero::to_string() const {
+    std::stringstream ss;
     ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
         << ", Precio: " << precio << ", Cantidad: " << cantidad
         << ", Marca: " << marca;
