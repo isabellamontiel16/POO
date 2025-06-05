194b6db3e5809cb99ffb6757ec60d67037ec5a61
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:20:03 -0600

    Update ProductoPerecedero.h

--- ProductoPerecedero.h
+++ ProductoPerecedero.h
@@ -7,11 +7,11 @@ class ProductoPerecedero : public Producto {
 private:
     double precio;
     int cantidad;
-    string fecha_caducidad;
+    std::string fecha_caducidad;
 
 public:
-    ProductoPerecedero(int _id, const string& _nombre, double _precio, int _cantidad, const string& _fecha);
-    string to_string() const override;
+    ProductoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _fecha);
+    std::string to_string() const override;
 };
 
 #endif
