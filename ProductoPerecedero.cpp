b0f79da0997942056c90111f3eacd078896a78b1
isabellamontiel16 <a01278286@tec.mx>
2025-06-05 01:20:28 -0600

    Update ProductoPerecedero.cpp

--- ProductoPerecedero.cpp
+++ ProductoPerecedero.cpp
@@ -1,12 +1,12 @@
 #include "ProductoPerecedero.h"
 #include <sstream>
 
-ProductoPerecedero::ProductoPerecedero(int _id, const string& _nombre, double _precio, int _cantidad, const string& _fecha)
+ProductoPerecedero::ProductoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _fecha)
     : Producto(_id, _nombre, "Perecedero"), precio(_precio), cantidad(_cantidad), fecha_caducidad(_fecha) {
 }
 
-string ProductoPerecedero::to_string() const {
-    stringstream ss;
+std::string ProductoPerecedero::to_string() const {
+    std::stringstream ss;
     ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
         << ", Precio: " << precio << ", Cantidad: " << cantidad
         << ", Caducidad: " << fecha_caducidad;
