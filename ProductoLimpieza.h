177a1fb2b5f22a00d2091c55fa80db3fadc166fd
isabellamontiel16 <a01278286@tec.mx>
2025-06-11 23:06:34 -0600

    Update ProductoLimpieza.h

--- ProductoLimpieza.h
+++ ProductoLimpieza.h
@@ -2,17 +2,26 @@
 #define PRODUCTOLIMPIEZA_H
 
 #include "Producto.h"
+#include "Inventario.h"
+#include <sstream>
 
 class ProductoLimpieza : public Producto {
 private:
-    double precio;
-    int cantidad;
+    Inventario inventario;
     std::string superficie;
 
 public:
-    ProductoLimpieza(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _superficie);
-    std::string to_string() const override;
+    ProductoLimpieza(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _superficie)
+        : Producto(_id, _nombre, "Limpieza"), inventario(_precio, _cantidad), superficie(_superficie) {
+    }
+
+    std::string to_string() const override {
+        std::stringstream ss;
+        ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
+            << ", " << inventario.to_string()
+            << ", Superficie: " << superficie;
+        return ss.str();
+    }
 };
 
-#endif
-
+#endif 
