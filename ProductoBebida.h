4567f904b9f360ef2db6854d5396dd0d70194040
isabellamontiel16 <a01278286@tec.mx>
2025-06-11 23:07:30 -0600

    Update ProductoBebida.h

--- ProductoBebida.h
+++ ProductoBebida.h
@@ -2,16 +2,26 @@
 #define PRODUCTOBEBIDA_H
 
 #include "Producto.h"
+#include "Inventario.h"
+#include <sstream>
 
 class ProductoBebida : public Producto {
 private:
-    double precio;
-    int cantidad;
+    Inventario inventario;
     bool alcohol;
 
 public:
-    ProductoBebida(int _id, const std::string& _nombre, double _precio, int _cantidad, bool _alcohol);
-    std::string to_string() const override;
+    ProductoBebida(int _id, const std::string& _nombre, double _precio, int _cantidad, bool _alcohol)
+        : Producto(_id, _nombre, "Bebida"), inventario(_precio, _cantidad), alcohol(_alcohol) {
+    }
+
+    std::string to_string() const override {
+        std::stringstream ss;
+        ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
+            << ", " << inventario.to_string()
+            << ", Alcohol: " << (alcohol ? "Sí" : "No");
+        return ss.str();
+    }
 };
 
-#endif
+#endif 
