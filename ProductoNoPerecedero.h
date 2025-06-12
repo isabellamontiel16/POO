818923223a32324f418c482202fdceb5d7ad8ac4
isabellamontiel16 <a01278286@tec.mx>
2025-06-11 23:08:10 -0600

    Update ProductoNoPerecedero.h

--- ProductoNoPerecedero.h
+++ ProductoNoPerecedero.h
@@ -2,16 +2,26 @@
 #define PRODUCTONOPERECEDERO_H
 
 #include "Producto.h"
+#include "Inventario.h"
+#include <sstream>
 
 class ProductoNoPerecedero : public Producto {
 private:
-    double precio;
-    int cantidad;
+    Inventario inventario;
     std::string marca;
 
 public:
-    ProductoNoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _marca);
-    std::string to_string() const override;
+    ProductoNoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _marca)
+        : Producto(_id, _nombre, "No Perecedero"), inventario(_precio, _cantidad), marca(_marca) {
+    }
+
+    std::string to_string() const override {
+        std::stringstream ss;
+        ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
+            << ", " << inventario.to_string()
+            << ", Marca: " << marca;
+        return ss.str();
+    }
 };
 
-#endif
+#endif 
