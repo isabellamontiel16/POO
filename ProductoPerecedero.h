f9279069d390adfe98abfa2beb5d7cbbfa6f5f47
isabellamontiel16 <a01278286@tec.mx>
2025-06-11 23:08:52 -0600

    Update ProductoPerecedero.h

--- ProductoPerecedero.h
+++ ProductoPerecedero.h
@@ -2,16 +2,26 @@
 #define PRODUCTOPERECEDERO_H
 
 #include "Producto.h"
+#include "Inventario.h"
+#include <sstream>
 
 class ProductoPerecedero : public Producto {
 private:
-    double precio;
-    int cantidad;
+    Inventario inventario;
     std::string fecha_caducidad;
 
 public:
-    ProductoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _fecha);
-    std::string to_string() const override;
+    ProductoPerecedero(int _id, const std::string& _nombre, double _precio, int _cantidad, const std::string& _fecha)
+        : Producto(_id, _nombre, "Perecedero"), inventario(_precio, _cantidad), fecha_caducidad(_fecha) {
+    }
+
+    std::string to_string() const override {
+        std::stringstream ss;
+        ss << "ID: " << id << ", Nombre: " << nombre << ", Tipo: " << tipo
+            << ", " << inventario.to_string()
+            << ", Caducidad: " << fecha_caducidad;
+        return ss.str();
+    }
 };
 
-#endif
+#endif 
